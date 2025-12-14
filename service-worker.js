6ea714804d092d1da12eadaa4bfc8024fea77897
heeyeonseo14-gif <heeyeonseo14@gmail.com>
2025-12-14 20:05:55 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // 先不做缓存，保证稳定
+});
