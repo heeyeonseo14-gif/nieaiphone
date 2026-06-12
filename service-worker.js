a13e04c9e596bd7fca3161f3f3f3260f36356d71
heeyeonseo14-gif <heeyeonseo14@gmail.com>
2026-06-12 21:34:11 +0800

    Refactor service worker for improved caching
    Refactor service worker to streamline event listeners and cache management.

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,19 @@
-const CACHE_NAME = "nieaiphone-v1";
+self.addEventListener(
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./home.html",
-  "./chat.html",
-  "./chat_ui.html",
-  "./chat_action.html",
-  "./ai_memory_v2.html",
-  "./api.html",
-  "./my_setting.html",
-  "./instagram.html",
-  "./music.html",
-  "./worldbook.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+"install",
 
-// 安装
-self.addEventListener("install", event => {
-  self.skipWaiting();
+()=>{
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+self.skipWaiting()
 
-// 激活
-self.addEventListener("activate", event => {
+}
 
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+)
 
-  self.clients.claim();
-});
+self.addEventListener(
 
-// fetch
-self.addEventListener("fetch", event => {
+"fetch",
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => response)
-      .catch(() => caches.match(event.request))
-  );
+()=>{}
 
-});
+)
