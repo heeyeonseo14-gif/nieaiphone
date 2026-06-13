3e32e9428b5bfed62c4f8e1505f6ea56a486d151
heeyeonseo14-gif <heeyeonseo14@gmail.com>
2026-06-13 17:44:02 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,47 @@
 self.addEventListener(
-
 "install",
-
 ()=>{
 
 self.skipWaiting()
 
 }
+)
+
+self.addEventListener(
+
+"activate",
+
+(event)=>{
+
+event.waitUntil(
+
+clients.claim()
 
 )
+
+}
+
+)
+
 
 self.addEventListener(
 
 "fetch",
 
-()=>{}
+(event)=>{
 
+event.respondWith(
+
+fetch(
+event.request,
+{
+cache:
+"no-store"
+}
+
+)
+
+)
+
+}
 )
